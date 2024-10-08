cfa8b5f0f4e23d1e2c785aae801469f74da6d5d0
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-10-08 17:09:11 -0400

    urls dashboard

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -5,7 +5,13 @@ from . import views
 urlpatterns = [
 
   path('sign-up', views.sign_up, name='sign-up'),
+  
+  path('sign-in', views.sign_in, name='sign-in'),
+  
+  path('sign-out', views.sign_out, name='sign-out'),
  
+  path('dashboard', views.dashboard, name='dashboard'),
+  
   # Email verification Url's 
  
   path('email-verification/<str:uidb64>/<str:token>/', views.email_verification, name='email-verification'),
