f1af0eb7f5469b3901de8e42bbd44c7517ca35d9
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-10-08 17:09:05 -0400

    create login form

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -1,9 +1,10 @@
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from django.contrib.auth.models import User
 
 from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 
 class CreateUserForm(UserCreationForm):
   email = forms.EmailField(required=True)
@@ -30,3 +31,8 @@ class CreateUserForm(UserCreationForm):
         raise forms.ValidationError('Email too long')
                
       return email
+
+
+class SignInForm(AuthenticationForm):
+  username = forms.CharField(widget=TextInput())
+  password = forms.CharField(widget=PasswordInput())
