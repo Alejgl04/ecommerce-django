d1cefc96417048433d174fb97b53d09195b47518
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-10-08 19:26:09 -0400

    form validations

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -2,23 +2,23 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from django.contrib.auth.models import User
-
-from django import forms
+from django.contrib.auth import authenticate
 from django.forms.widgets import PasswordInput, TextInput
 
+from django import forms
+
 class CreateUserForm(UserCreationForm):
-  email = forms.EmailField(required=True)
+  
+  def __init__(self, *args, **kwargs):
+    super(CreateUserForm, self).__init__(*args, **kwargs)
+    
+    self.fields['email'].required = True
+    
   class Meta:
     
     model = User
     fields = ['username', 'email', 'password1', 'password2']
-    
-    def __init__(self, *args, **kwargs):
-      super(CreateUserForm, self).__init__(*args, **kwargs)
       
-      # self.fields['email'].required = True
-      
-    
     def clean_email(self):
       email = self.cleaned_data.get('email')
       
@@ -34,5 +34,33 @@ class CreateUserForm(UserCreationForm):
 
 
 class SignInForm(AuthenticationForm):
+  
   username = forms.CharField(widget=TextInput())
   password = forms.CharField(widget=PasswordInput())
+  
+  def clean(self):
+    username = self.cleaned_data.get('username')
+    password = self.cleaned_data.get('password')
+    
+    user = authenticate(username=username, password=password)
+    
+    if not user or not user.is_active:
+    
+      raise forms.ValidationError("Sorry, that credentials was invalid. Please try again.")
+    
+    return self.cleaned_data
+
+
+class UpdateUserForm(forms.ModelForm):
+  password = None
+  
+  def __init__(self, *args, **kwargs):
+    super(UpdateUserForm, self).__init__(*args, **kwargs)
+  
+    self.fields['email'].required = True
+    
+  class Meta:
+    
+    model = User
+    fields = ['username', 'email']
+    exclude = ['password1', 'password1']
