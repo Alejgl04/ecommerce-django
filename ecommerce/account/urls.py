f6f6377502013b7f1b5722359fe32d528cbb7107
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-10-10 18:18:08 -0400

    reset password

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -28,6 +30,21 @@ urlpatterns = [
   path('email-verification-success', views.email_verification_success, name='email-verification-success'),
   
   path('email-verification-failed', views.email_verification_failed, name='email-verification-failed'),
+  
+  
+  # Password forms urls/views 
+  
+  # 1 Submit email form
+  path('reset_password', auth_views.PasswordResetView.as_view(template_name='account/password/password-reset.html'), name='reset_password'),
+  
+  #2 Success manage stating that a password reset was sent
+  path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name='account/password/password-reset-sent.html'), name='password_reset_done'),
+  
+  # #3 Password reset link
+  path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password/password-reset-form.html'), name='password_reset_confirm'),
+  
+  # #4 Success message
+  path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name='account/password/password-reset-complete.html'), name='password_reset_complete')
   
   
   
