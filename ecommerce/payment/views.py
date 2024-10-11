1e4c493d393cec4e652c4b3abcbbeb7620887027
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-10-11 18:53:42 -0400

    checkout view

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 
+
+
+from . models import ShippingAddress
+
 # Create your views here.
 
 def checkout( request ):
+
+  if request.user.is_authenticated:
+    
+    try:     
+      # authenticated users with shipping info
+      shipping_address = ShippingAddress.objects.get(user=request.user.id)
+      
+      context = { 'shipping': shipping_address }
+      
+      return render( request, 'payment/checkout.html', context )
+
+    except:
+      # Authenticated users with no shipping information
+      return render( request, 'payment/checkout.html' )
+
   return render( request, 'payment/checkout.html' )
+
+
+
 
 def payment_success( request ):
   return render( request, 'payment/payment-success.html')
